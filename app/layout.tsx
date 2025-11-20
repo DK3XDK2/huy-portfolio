3252796b205870db8927312757a3f43a4ab31f19
Vu Quang Huy <hacktyper12@gmail.com>
2025-11-20 12:46:54 +0700

    nice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Vũ Quang Huy | Portfolio",
-  description: "Portfolio cá nhân  Vũ Quang Huy - Fullstack Web Developer.",
+  description: "Portfolio cá nhân Vũ Quang Huy - Web Developer.",
 };
 
 export default function RootLayout({
@@ -15,10 +18,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const url = new URL(window.location.href);
+
+    if (url.searchParams.has("fbclid")) {
+      url.searchParams.delete("fbclid");
+      window.history.replaceState({}, "", url.toString());
+    }
+  }, []);
+
   return (
     <html lang="vi">
       <body className={`${inter.className} bg-black text-slate-100`}>
-        {}
         <div className="pointer-events-none fixed inset-0 overflow-hidden">
           <div className="absolute -top-40 -left-20 w-[600px] h-[600px] bg-fuchsia-600/10 rounded-full blur-[120px]" />
           <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-cyan-500/10 rounded-full blur-[140px]" />
@@ -29,11 +40,11 @@ export default function RootLayout({
 
           <main
             className="
-    flex-1 
-    px-4 sm:px-8 md:px-16 lg:px-32 
-    py-10
-    bg-gradient-to-br from-slate-900 via-slate-950 to-black
-  "
+              flex-1 
+              px-4 sm:px-8 md:px-16 lg:px-32 
+              py-10
+              bg-gradient-to-br from-slate-900 via-slate-950 to-black
+            "
           >
             {children}
           </main>
